0b2676168f4e24ab7a3800e2667d38e266e8205f
BrnDavide <brn.davide@gmail.com>
2024-01-30 16:42:06 +0100

    aggiunte

---
+++ Lezione1/Operazioni.java
@@ -0,0 +1,20 @@
+public class Operazioni{
+    public static void main (String args[]){
+        int a=64;
+        int b=38;
+        double c=61.4;
+        double d=26.8;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        System.out.println(a+c);
+        System.out.println(a-c);
+        System.out.println(a*c);
+        System.out.println(a/c);
+        System.out.println(a%c);        
+
+    }
+}
