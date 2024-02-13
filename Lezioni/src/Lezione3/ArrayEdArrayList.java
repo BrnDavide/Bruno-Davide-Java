4919fb4d9038268cf4081cdec4a14c51790c1460
BrnDavide <brn.davide@gmail.com>
2024-02-13 09:21:31 +0100

    COMMIT

--- Lezioni/src/Lezione3/ArrayEdArrayList.java
+++ Lezioni/src/Lezione3/ArrayEdArrayList.java
@@ -3,7 +3,7 @@ package Lezione3;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ArrayList {
+public class ArrayEdArrayList {
     public static void main(String[] args) {
         List<Integer> numeri= new ArrayList<>();
         numeri.add(1);
@@ -11,6 +11,6 @@ public class ArrayList {
         System.out.println(numeri); //Stampa tutto
         System.out.println(numeri.get(0)); //Stampa elemento nell'indice specificato
         numeri.remove(1);//Questo comando elimina elementi nell'indice.
-        System.out.println(numeri.size); // Equivale al length.
+        System.out.println(numeri.size()); // Equivale al length.
     }
 }
--- Lezioni/src/Lezione3/Esercizi.java
+++ Lezioni/src/Lezione3/Esercizi.java
@@ -1,7 +1,49 @@
 package Lezione3;
-
+import java.util.Scanner;
 public class Esercizi {
     public static void main(String[] args) {
-        /**/
+        Scanner tastiera = new Scanner(System.in);
+        /*
+        * Es1. (if-else-Scanner) Chiedere un numero e di stampare pari o dispari.
+        *  */
+        /*System.out.println("inserisci un numero");
+        int numero= Integer.parseInt(tastiera.nextLine());
+        if (numero %2==0){
+            System.out.println("Il numero è pari");
+        }
+        else {
+            System.out.println("Il numero è dispari");
+        }*/
+
+        /*Es2: (for) Chiedere quante temperature si vuole inserire
+        stampare il minimo, massimo e la media (senza array).*/
+
+        System.out.println("Quante temperature vuoi inserire?");
+        int indexTemp=Integer.parseInt(tastiera.nextLine());
+        for (int i=0;i<indexTemp;i++){
+
+            System.out.println("Inserisci temperatura "+ indexTemp);
+        }
+
+        System.out.println("Inserisci temp 1");
+        double t1= Integer.parseInt(tastiera.nextLine());
+        System.out.println("Inserisci temp 2");
+        double t2= Integer.parseInt(tastiera.nextLine());
+        System.out.println("Inserisci temp 3");
+        double t3= Integer.parseInt(tastiera.nextLine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
